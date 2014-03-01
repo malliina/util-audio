187e6f6c8f8592e01b2ca4ff7e53841831690109
malliina <malliina123@gmail.com>
2014-03-01 19:00:54 +0100

    version upd

--- project/AudioBuild.scala
+++ project/AudioBuild.scala
@@ -3,18 +3,17 @@ import sbt.Keys._
 import Dependencies._
 
 object AudioBuild extends Build {
-  lazy val utilAudio = Project("util-audio", file("."), settings = commonSettings)
-    .settings(
-    libraryDependencies ++= Seq(utilDep, scalaTest, jodaTime, jodaConvert, jAudioTagger),
-    exportJars := true
-  )
+  lazy val utilAudio = Project("util-audio", file(".")).settings(commonSettings: _*)
 
-  lazy val commonSettings = Defaults.defaultSettings ++ Seq(
+  lazy val commonSettings = Seq(
     version := "1.0.0",
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.3",
     retrieveManaged := false,
     sbt.Keys.fork in Test := true,
-    resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-    resolvers += "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/"
+    libraryDependencies ++= Seq(utilDep, scalaTest, jodaTime, jodaConvert, jAudioTagger),
+    exportJars := true,
+    resolvers ++= Seq(
+      "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases/")
   )
 }
--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -8,11 +8,10 @@ import sbt.Keys._
 object BuildBuild extends Build {
   // "build.sbt" goes here
   override lazy val settings = super.settings ++ Seq(
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.3",
     resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
     resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-    scalacOptions ++= Seq("-unchecked", "-deprecation"),
-    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
+    scalacOptions ++= Seq("-unchecked", "-deprecation")
   )
 
   override lazy val projects = Seq(root)
